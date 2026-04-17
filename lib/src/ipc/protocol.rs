bab7c08c57d73474598f6f397a1a985178a8af7e
NonExistPlayer <nonexistplayer@gmail.com>
2026-04-17 22:50:30 +0300

    fix(lib/ipc): protocol

--- lib/src/ipc/protocol.rs
+++ lib/src/ipc/protocol.rs
@@ -5,34 +5,33 @@
  *  SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-use std::io::{Read, Write};
+use std::io::{BufRead, BufReader, Write};
 use std::os::unix::net::UnixStream;
 
 use anyhow::Result;
 use serde::{Deserialize, Serialize, de::DeserializeOwned};
 
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 pub enum Request {
     Ping,
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 pub enum Response {
     Pong,
 }
 
-pub fn send<T: Serialize>(mut stream: &UnixStream, serializable: T) -> Result<()> {
+pub fn send<T: Serialize>(stream: &UnixStream, serializable: T) -> Result<()> {
     let json = serde_json::to_string(&serializable)?;
-
-    writeln!(stream, "{json}")?;
-
+    let mut writer = stream;
+    writeln!(writer, "{json}")?;
     Ok(())
 }
 
-pub fn read<T: DeserializeOwned>(mut stream: &UnixStream) -> Result<T> {
+pub fn read<T: DeserializeOwned>(stream: &UnixStream) -> Result<T> {
     let mut json = String::new();
-
-    stream.read_to_string(&mut json)?;
+    let mut reader = BufReader::new(stream);
+    reader.read_line(&mut json)?;
 
     let deserializable: T = serde_json::from_str(&json)?;
 
